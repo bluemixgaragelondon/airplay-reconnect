8d26a42d7b40c9fd77d3178fb3e9091f16443631
Holly Cummins <pair+holly@uk.ibm.com>
2016-01-07 13:00:30 +0000

    Switch to monitor mode packet sniffing

--- airplay.sh
+++ airplay.sh
@@ -6,8 +6,18 @@ isinuse() {
 	# information, but by design doesn't return until killed, so use ping. 
 	ipaddress=$(ping -c 1 $tvhostname | awk -F'[()]' '/PING/{print $2}')
 	echo About to sniff traffic to $tvhostname \($ipaddress\)
+  arp -a &> /var/tmp/arp-output &
+  pid=$!
+  sleep 10
+  kill $pid
+  fieldindex='$4'
+  # Parse something of the form ? (10.37.109.150) at 40:33:1a:3d:e6:ee on en0 ifscope [ethernet] ? (10.37.109.158) at 34:2:86:56:b6:8e on en0 ifscope [ethernet]
+  # The awk quotes get a bit messy with the variable substitution, so split the expression up
+  echo Parsing mac address from line `awk -F"[ ]" "/\($ipaddress\)/{print}" /var/tmp/arp-output`
+  macaddress=`awk -F"[ ]" "/($ipaddress)/{print $fieldindex}" /var/tmp/arp-output`
+ echo Looking for traffic to mac address $macaddress 
   # Make sure that the user running this script has passwordless sudo tcpdump
-	sudo tcpdump tcp port 7000 and host $ipaddress &> /var/tmp/airplay-tcpdump-output &
+	sudo tcpdump -i en0 -I ether dst $macaddress &> /var/tmp/airplay-tcpdump-output &
 	# Get the PID of the tcpdump command
 	pid=$!
 	# Capture 10 seconds of output, then kill the job
@@ -17,7 +27,8 @@ isinuse() {
 	packetcount=`awk -F'[ ]' '/captured/{print $1}' /var/tmp/airplay-tcpdump-output`
 	echo Finished sniffing packets - there were $packetcount. 
 	
-	if [ $packetcount -gt 0 ]
+  # There will be a lot of packets flying around, so the bar for in-use is high
+	if [ $packetcount -gt 20 ]
         # 0 is true and 1 is false in bash-world
 	then
 		echo Apple TV is in use. 
